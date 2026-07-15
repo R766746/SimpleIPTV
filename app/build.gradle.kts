3fc03d76276b34d323399211af68ef5fe55c32bb
R766746 <154349449+R766746@users.noreply.github.com>
2026-07-15 02:28:00 -0400

    Add navigation foundation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,18 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    // Standard Compose
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
+    // Navigation & Hilt
+    implementation(libs.androidx.navigation.compose)
+
+
 }
